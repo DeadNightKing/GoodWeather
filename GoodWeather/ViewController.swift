e89db400bd18fac2b683f63c1ea58a05c553ae06
RyanChou <ryanchou@welltend.com.tw>
2022-07-21 13:55:09 +0800

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Flow_RyanChou on 2022/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
